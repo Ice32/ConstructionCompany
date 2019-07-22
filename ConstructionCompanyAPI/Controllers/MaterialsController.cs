cf7cb7a77f9f91f3f44c1dfbd5992346cb2b530b
Kenan Selimović <kenan.selimovic@edu.fit.ba>
2019-07-22 19:18:49 +0200

    Implement MaterialsController authorization

--- ConstructionCompanyAPI/Controllers/MaterialsController.cs
+++ ConstructionCompanyAPI/Controllers/MaterialsController.cs
@@ -3,6 +3,8 @@ using ConstructionCompany.BR;
 using ConstructionCompanyAPI.Controllers.Generics;
 using ConstructionCompanyDataLayer.Models;
 using ConstructionCompanyModel.ViewModels.Worksheets;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ConstructionCompanyAPI.Controllers
 {
@@ -10,6 +12,20 @@ namespace ConstructionCompanyAPI.Controllers
     {
         public MaterialsController(ICRUDService<Material, object> service, IMapper mapper) : base(service, mapper)
         {
+        }
+        
+        [HttpPost]
+        [Authorize(Roles="Manager")]
+        public override MaterialVM Insert(MaterialAddVM request)
+        {
+            return base.Insert(request);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles="Manager")]
+        public override MaterialVM Update(int id, MaterialAddVM request)
+        {
+            return base.Update(id, request);
         }
     }
 }
